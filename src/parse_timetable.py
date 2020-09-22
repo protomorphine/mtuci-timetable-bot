e874582a369c1dfc07ba9726a6c272e5c825aee9
Daniil Zaitsev <kupernewfag@gmail.com>
2020-09-22 22:22:54 +0300

    make it better

--- src/parse_timetable.py
+++ src/parse_timetable.py
@@ -2,10 +2,10 @@ from datetime import datetime
 import json
 
 
-TEMPLATE = open(".output_template", "r", encoding="utf-8").read()
-WEEK = open(".json/week.json", "r", encoding="utf-8").read()
-PARRITY_DAYS = open(".json/parity_days.json", "r", encoding="utf-8").read()
-WEEK_DAYS_RU = open(".json/week_days_ru.json", "r", encoding="utf-8").read()
+TEMPLATE = open("./output_template", "r", encoding="utf-8").read()
+WEEK = open("./json/week.json", "r", encoding="utf-8").read()
+PARRITY_DAYS = open("./json/parity_days.json", "r", encoding="utf-8").read()
+WEEK_DAYS_RU = open("./json/week_days_ru.json", "r", encoding="utf-8").read()
 
 
 def get_day_name(num):
